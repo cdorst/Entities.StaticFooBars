82b2884acfb20503946e1c46d3aae90581f6971f
Christopher Dorst <chris@dorst.email>
2018-03-27 01:22:29 -0500

    Add StaticFooBar.cs

---
+++ Entities.StaticFooBars/StaticFooBar.cs
@@ -0,0 +1,43 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
+using DevOps.Code.Entities.Interfaces.Entity;
+using Position = ProtoBuf.ProtoMemberAttribute;
+using ProtoBufSerializable = ProtoBuf.ProtoContractAttribute;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq.Expressions;
+
+namespace Entities.StaticFooBars
+{
+    /// <summary>Contains the StaticFooBar entity type</summary>
+    [ProtoBufSerializable]
+    [Table("StaticFooBars", Schema = "EntitiesStaticFooBars")]
+    public class StaticFooBar : IStaticEntity<int>
+    {
+        public StaticFooBar()
+        {
+        }
+
+        public StaticFooBar(int baz)
+        {
+            Baz = baz;
+        }
+
+        /// <summary>Contains Baz value</summary>
+        [Position(2)]
+        public int Baz { get; set; }
+
+        /// <summary>StaticFooBar unique identifier (primary key)</summary>
+        [Key]
+        [Position(1)]
+        public int StaticFooBarId { get; set; }
+
+        /// <summary>Returns a value that uniquely identifies this entity type. Each entity type in the model has a unique identifier.</summary>
+        public int GetEntityType() => 4;
+
+        /// <summary>Returns the entity's unique identifier.</summary>
+        public int GetKey() => StaticFooBarId;
+    }
+}
